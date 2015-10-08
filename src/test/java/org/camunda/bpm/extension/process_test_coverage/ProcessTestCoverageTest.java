18d69e5432052d091288fec6af882986af8c9d4d
Falko Menge <falko.menge@camunda.com>
2015-10-08 15:36:53 +0200

    Add algorithm to exclude non-executable processes (WIP)

--- src/test/java/org/camunda/bpm/extension/process_test_coverage/ProcessTestCoverageTest.java
+++ src/test/java/org/camunda/bpm/extension/process_test_coverage/ProcessTestCoverageTest.java
@@ -1,5 +1,6 @@
 package org.camunda.bpm.extension.process_test_coverage;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +8,9 @@ import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineTestCase;
 import org.camunda.bpm.extension.process_test_coverage.ProcessTestCoverage;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.camunda.bpm.model.bpmn.instance.FlowNode;
+import org.camunda.bpm.model.bpmn.instance.Process;
 
 /**
  * Test case starting an in-memory database-backed Process Engine.
@@ -49,6 +53,34 @@ public class ProcessTestCoverageTest extends ProcessEngineTestCase {
     
     // calculate coverage for this method, but also add to the overall coverage of the process
     ProcessTestCoverage.calculate(processInstance.getId(), processEngine);
+  }
+
+  @Deployment(resources = "collaboration-with-non-executable-process.bpmn")
+  public void testCollaborationWithNonExecutableProcess() {
+    String processDefinitionKey = "Process_1";
+    String processDefinitionId = repositoryService.createProcessDefinitionQuery().processDefinitionKey(processDefinitionKey).latestVersion().singleResult().getId();
+    BpmnModelInstance model = repositoryService.getBpmnModelInstance(processDefinitionId);
+//    assertNull(model.getModelElementById("StartEvent_2"));
+    Collection<Process> processes = model.getModelElementsByType(Process.class);
+    Process theProcess = null;
+    for (Process process : processes) {
+      if (process.getId().equals(processDefinitionKey)) {
+        theProcess = process;
+        break;
+      }
+    }
+    assertNotNull(theProcess);
+    Collection<FlowNode> flowNodes = theProcess.getChildElementsByType(FlowNode.class);
+    FlowNode startEvent3 = null;
+    for (FlowNode flowNode : flowNodes) {
+      if (flowNode.getId().equals("StartEvent_2")) {
+        fail();
+      }
+      if (flowNode.getId().equals("StartEvent_3")) {
+        startEvent3 = flowNode;
+      }
+    }
+    assertNull(startEvent3); // TODO recurse into sub-processes
   }
   
 }
