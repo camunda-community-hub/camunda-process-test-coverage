19621d60c6b25c99937796db8c4487c5583c2377
Axel Gross <axel.gross@wdw-elab.de>
2016-02-25 13:22:23 +0100

    cleanup and update pom to 0.2.5
    * removed comments 
    * formatting
    * removed dead code
    * rename ProcessTestCoverage to ProcessTestCoverageCalculator

--- src/main/java/org/camunda/bpm/extension/process_test_coverage/junit/rules/TestCoverageTestRunState.java
+++ src/main/java/org/camunda/bpm/extension/process_test_coverage/junit/rules/TestCoverageTestRunState.java
@@ -1,11 +1,9 @@
 package org.camunda.bpm.extension.process_test_coverage.junit.rules;
 
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Logger;
 
-import org.camunda.bpm.extension.process_test_coverage.TraceActivitiesHistoryEventHandler;
 import org.camunda.bpm.extension.process_test_coverage.trace.CoveredActivity;
 import org.camunda.bpm.extension.process_test_coverage.trace.CoveredElement;
 import org.camunda.bpm.extension.process_test_coverage.trace.CoveredElements;
@@ -17,23 +15,35 @@ public class TestCoverageTestRunState {
     
 	Logger log = Logger.getLogger(TestCoverageTestRunState.class.getCanonicalName());
 	
-	/** NonNull global variable holding a list of the flow trace elements of the test run.<br>
-	 * clear this (e.g. in @BeforeClass) if you want to */
-	Set<CoveredElement> currentFlowTrace = new HashSet<CoveredElement>();
-	Set<String> relevantDeploymentIds = new HashSet<String>();
-	
-	public Set<String> getRelevantDeploymentIds() {
-        return relevantDeploymentIds;
+
+    private static final ThreadLocal<TestCoverageTestRunState> threadLocalInstance =
+             new ThreadLocal<TestCoverageTestRunState>() {
+                 @Override protected TestCoverageTestRunState initialValue() {
+                     return new TestCoverageTestRunState();
+             }
+         }; 
+    public static TestCoverageTestRunState INSTANCE() {
+        return threadLocalInstance.get();
     }
 	
+	/** NonNull global variable holding a list of the flow trace elements of the coverage's tests run.<br>
+	 * clear this (e.g. in @BeforeClass) if you want to start collecting coverage */
+	Set<CoveredElement> currentFlowTrace = new HashSet<CoveredElement>();
+	
+	/** NonNull global variable holding a list relevant deployment id's of the coverage's tests run.<br>
+     * clear this (e.g. in @BeforeClass) if you want to start collecting coverage */
+	Set<String> relevantDeploymentIds = new HashSet<String>();
+	
 	double highestSeenCoverage = COVERAGE_NOT_SET;
-	/** clears the current flow trace, e.g. in @BeforeClass */
+	
+	/** clears the current trace information, e.g. in @BeforeClass */
 	public void resetCurrentFlowTrace(){
-	    log.info("Resetting complete flow trace");
-		currentFlowTrace.clear(); // FIXME
+	    log.info("Resetting complete flow trace, relevantDeploymentIds and highest seen coverage");
+		currentFlowTrace.clear();
 		relevantDeploymentIds.clear();
 		highestSeenCoverage = COVERAGE_NOT_SET;
 	}
+	
 	public void notifyCoveredElement(/*@NotNull*/ CoveredElement coveredElement) {
 		log.info("notifyCoveredElement(" + coveredElement + ")");
 		currentFlowTrace.add(coveredElement);
@@ -58,16 +68,9 @@ public class TestCoverageTestRunState {
 		return CoverageMappings.mapElementsToIds(CoveredElements.findProcessInstances(null, CoveredActivity.class, currentFlowTrace));
 	}
 	
-	private static final ThreadLocal<TestCoverageTestRunState> threadLocalInstance =
-	         new ThreadLocal<TestCoverageTestRunState>() {
-	             @Override protected TestCoverageTestRunState initialValue() {
-	                 return new TestCoverageTestRunState();
-	         }
-	     }; 
-	private static final TestCoverageTestRunState theInstance = new TestCoverageTestRunState();
-	public static TestCoverageTestRunState INSTANCE() {
-		return threadLocalInstance.get();
-	}
+    public Set<String> getRelevantDeploymentIds() {
+        return relevantDeploymentIds;
+    }
     
 	public boolean isHighestSeenCoverageSet() {
         return highestSeenCoverage != COVERAGE_NOT_SET;
