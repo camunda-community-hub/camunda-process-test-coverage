5c29c0e693be537a047ca649268c5cc4449972a3
Martin Schimak <martin.schimak@plexiti.com>
2017-03-29 10:58:03 +0200

    test(coverage-ratio): adapt test cases and bpmn to work with Camunda 7.3 and 7.2
    Closes #19

--- test/src/test/java/org/camunda/bpm/extension/process_test_coverage/model/CoverageRatioTest.java
+++ test/src/test/java/org/camunda/bpm/extension/process_test_coverage/model/CoverageRatioTest.java
@@ -3,43 +3,22 @@ package org.camunda.bpm.extension.process_test_coverage.model;
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.variable.Variables;
 import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRule;
 import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
-import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * Test case starting an in-memory database-backed Process Engine.
  */
-@Deployment(resources = "coverage-ratio-test.bpmn")
 public class CoverageRatioTest {
 
     @Rule
     @ClassRule
     public static TestCoverageProcessEngineRule rule
-        = TestCoverageProcessEngineRuleBuilder.create().build();
-
-    private Map<String, Object> variables = new HashMap<String, Object>();
-
-    @Before
-    public void init() {
-        List all = new ArrayList();
-        all.add(1);
-        all.add(2);
-        variables.put("all", all);
-        variables.put("ok", true);
-    }
-
-    public void startProcess() {
-        rule.getRuntimeService().startProcessInstanceByKey("testCoverageRatio", variables);
-    }
+        = TestCoverageProcessEngineRuleBuilder.create().assertClassCoverageAtLeast(1.0).build();
 
     public void completeTask() {
         TaskService taskService = rule.getProcessEngine().getTaskService();
@@ -48,15 +27,16 @@ public class CoverageRatioTest {
     }
 
     @Test
-    public void testClassOk() {
-        startProcess();
+    @Deployment(resources = "coverage-ratio-test.bpmn")
+    public void testCoverageRatio_Ok() {
+        rule.getRuntimeService().startProcessInstanceByKey("testCoverageRatio", Variables.createVariables().putValue("ok", true));
         completeTask();
     }
 
     @Test
-    public void testClassNotOk() {
-        variables.put("ok", false);
-        startProcess();
+    @Deployment(resources = "coverage-ratio-test.bpmn")
+    public void testCoverageRatio_NotOk() {
+        rule.getRuntimeService().startProcessInstanceByKey("testCoverageRatio", Variables.createVariables().putValue("ok", false));
         completeTask();
     }
 
