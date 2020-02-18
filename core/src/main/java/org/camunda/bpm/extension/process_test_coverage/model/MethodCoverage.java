036b3ef6f154a2f97600112ac99b501e3779d755
r.n.o.aliev <r.n.o.aliev@gmail.com>
2020-02-18 22:54:05 +0300

    small fix: import Collections

--- core/src/main/java/org/camunda/bpm/extension/process_test_coverage/model/MethodCoverage.java
+++ core/src/main/java/org/camunda/bpm/extension/process_test_coverage/model/MethodCoverage.java
@@ -1,6 +1,7 @@
 package org.camunda.bpm.extension.process_test_coverage.model;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
