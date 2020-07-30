568dea90102837d451c891965d36379b9d94c46c
ov7a <ov7a@yandex.ru>
2020-07-30 12:32:21 +0300

    Report types can now be selected for rule. Fixes #54

--- core/src/main/java/org/camunda/bpm/extension/process_test_coverage/junit/rules/TestCoverageProcessEngineRuleBuilder.java
+++ core/src/main/java/org/camunda/bpm/extension/process_test_coverage/junit/rules/TestCoverageProcessEngineRuleBuilder.java
@@ -94,6 +94,26 @@ public class TestCoverageProcessEngineRuleBuilder {
     }
 
     /**
+     * Configures whenever test method coverage handling is needed.
+     * @param needHandleTestMethodCoverage boolean
+     * @return
+     */
+    public TestCoverageProcessEngineRuleBuilder handleTestMethodCoverage(boolean needHandleTestMethodCoverage) {
+        rule.setHandleTestMethodCoverage(needHandleTestMethodCoverage);
+        return this;
+    }
+
+    /**
+     * Configures whenever class coverage handling is needed.
+     * @param needHandleClassCoverage boolean
+     * @return
+     */
+    public TestCoverageProcessEngineRuleBuilder handleClassCoverage(boolean needHandleClassCoverage) {
+        rule.setHandleClassCoverage(needHandleClassCoverage);
+        return this;
+    }
+
+    /**
      * Configures CoverageTestRunStateFactory used to create CoverageTestRunState. Useful for sharing state between several test-classes
      * @param coverageTestRunStateFactory
      * @return
