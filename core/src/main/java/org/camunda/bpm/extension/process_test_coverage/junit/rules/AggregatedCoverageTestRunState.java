03c86b210a4ba6285db8f9779a1878839ee8b405
Simon Zambrovski <simon.zambrovski@holisticon.de>
2020-10-15 18:32:19 +0200

    feature/bugfix: removed a side-effect of the getter making evaluation on the class level impossible, fix #68

--- core/src/main/java/org/camunda/bpm/extension/process_test_coverage/junit/rules/AggregatedCoverageTestRunState.java
+++ core/src/main/java/org/camunda/bpm/extension/process_test_coverage/junit/rules/AggregatedCoverageTestRunState.java
@@ -2,35 +2,49 @@ package org.camunda.bpm.extension.process_test_coverage.junit.rules;
 
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.repository.ProcessDefinition;
-import org.camunda.bpm.extension.process_test_coverage.model.*;
+import org.camunda.bpm.extension.process_test_coverage.model.AggregatedClassCoverage;
+import org.camunda.bpm.extension.process_test_coverage.model.AggregatedCoverage;
+import org.camunda.bpm.extension.process_test_coverage.model.ClassCoverage;
+import org.camunda.bpm.extension.process_test_coverage.model.CoveredElement;
+import org.camunda.bpm.extension.process_test_coverage.model.MethodCoverage;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Coverage test run state used to hold results for multiple classes.
+ *
  * @author ov7a
  */
 public class AggregatedCoverageTestRunState implements CoverageTestRunState {
 
     private CoverageTestRunState currentCoverageTestRunState = null;
 
-    private List<ClassCoverage> classCoverages = new ArrayList<ClassCoverage>();
+    private final List<ClassCoverage> classCoverages = new ArrayList<>();
 
-    public void switchToNewState(CoverageTestRunState newState){
+    public void switchToNewState(CoverageTestRunState newState) {
         finishState();
         currentCoverageTestRunState = newState;
     }
 
-    private void finishState(){
-        if (currentCoverageTestRunState != null){
+    private void finishState() {
+        if (currentCoverageTestRunState != null) {
             classCoverages.add(currentCoverageTestRunState.getClassCoverage());
             currentCoverageTestRunState = null;
         }
     }
 
-    public AggregatedCoverage getAggregatedCoverage(){
-        finishState();
-        return new AggregatedClassCoverage(classCoverages);
+    /**
+     * Retrieves the aggregated coverage without changing the current state.
+     *
+     * @return aggregated coverage.
+     */
+    public AggregatedCoverage getAggregatedCoverage() {
+        final List<ClassCoverage> aggregatedClassCoverage = new ArrayList<>(classCoverages);
+        if (currentCoverageTestRunState != null) {
+            aggregatedClassCoverage.add(currentCoverageTestRunState.getClassCoverage());
+        }
+        return new AggregatedClassCoverage(aggregatedClassCoverage);
     }
 
     @Override
