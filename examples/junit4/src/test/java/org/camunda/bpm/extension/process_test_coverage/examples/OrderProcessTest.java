cd4eb5683ccd108c2199e495ce4650425987854b
Dominik Horn <dominik.horn93@gmail.com>
2020-12-13 11:07:22 +0100

    feat: add junit4 example

---
+++ examples/junit4/src/test/java/org/camunda/bpm/extension/process_test_coverage/examples/OrderProcessTest.java
@@ -0,0 +1,56 @@
+package org.camunda.bpm.extension.process_test_coverage.examples;
+
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
+
+@Deployment(resources = "order-process.bpmn")
+public class OrderProcessTest {
+
+    @Rule
+    @ClassRule
+    public static final ProcessEngineRule processEngineRule = TestCoverageProcessEngineRuleBuilder.create().build();
+
+    @Test
+    public void testHappyPath() {
+        final ProcessInstance instance = this.startProcess();
+
+        assertThat(instance).isWaitingAt("Task_ProcessOrder");
+
+        complete(task(), withVariables("orderOk", true));
+
+        assertThat(instance).isWaitingAt("Task_DeliverOrder");
+
+        complete(task());
+
+        assertThat(instance)
+                .hasPassed("Event_OrderProcessed")
+                .isEnded();
+    }
+
+    @Test
+    public void testCancelOrder() {
+        final ProcessInstance instance = this.startProcess();
+
+        assertThat(instance).isWaitingAt("Task_ProcessOrder");
+
+        complete(task(), withVariables("orderOk", false));
+
+        assertThat(instance)
+                .hasPassed("Event_OrderCancelled")
+                .isEnded();
+    }
+
+
+    private ProcessInstance startProcess() {
+        return runtimeService().startProcessInstanceByKey("order-process");
+    }
+
+
+}
