776110830b93277c69a41054d94d603f789e2dbc
Jan Rohwer <jan@rohwer.online>
2022-05-11 22:31:15 +0200

    feat: #198 refactor platform 8 event creation logic

--- extension/engine-platform-8/src/main/kotlin/org/camunda/community/process_test_coverage/engine/platform8/ZeebeEventCreator.kt
+++ extension/engine-platform-8/src/main/kotlin/org/camunda/community/process_test_coverage/engine/platform8/ZeebeEventCreator.kt
@@ -1,31 +1,39 @@
 package org.camunda.community.process_test_coverage.engine.platform8
 
 import io.camunda.zeebe.process.test.assertions.BpmnAssert
-import io.camunda.zeebe.process.test.filters.RecordStream
 import io.camunda.zeebe.protocol.record.RecordType
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent
 import io.camunda.zeebe.protocol.record.value.BpmnElementType
+import io.camunda.zeebe.protocol.record.value.ProcessInstanceRecordValue
 import org.camunda.community.process_test_coverage.core.model.Collector
 import org.camunda.community.process_test_coverage.core.model.Event
 import org.camunda.community.process_test_coverage.core.model.EventSource
 import org.camunda.community.process_test_coverage.core.model.EventType
 
+/**
+ * Creates events for the collector from the record stream of the zeebe process engine.
+ */
 fun createEvents(collector: Collector, lastEventTime: Long) {
     BpmnAssert.getRecordStream().processInstanceRecords()
-        .filter { it.value.bpmnElementType == BpmnElementType.SEQUENCE_FLOW && it.recordType == RecordType.EVENT }
+        .filter { it.recordType == RecordType.EVENT }
         .filter { it.timestamp > lastEventTime }
-        .forEach { collector.addEvent(Event(EventSource.SEQUENCE_FLOW, EventType.TAKE, it.value.elementId,
-            it.value.bpmnElementType.elementTypeName.orElseGet { "" }, it.value.bpmnProcessId, it.timestamp)) }
-    BpmnAssert.getRecordStream().processInstanceRecords()
-        .filter { it.recordType == RecordType.EVENT && it.intent == ProcessInstanceIntent.ELEMENT_ACTIVATED
-                && it.value.bpmnElementType != BpmnElementType.PROCESS }
-        .filter { it.timestamp > lastEventTime }
-        .forEach { collector.addEvent(Event(EventSource.FLOW_NODE, EventType.START, it.value.elementId,
-            it.value.bpmnElementType.elementTypeName.orElseGet { "" }, it.value.bpmnProcessId, it.timestamp)) }
-    BpmnAssert.getRecordStream().processInstanceRecords()
-        .filter { it.recordType == RecordType.EVENT && it.intent == ProcessInstanceIntent.ELEMENT_COMPLETED
-                && it.value.bpmnElementType != BpmnElementType.PROCESS }
-        .filter { it.timestamp > lastEventTime }
-        .forEach { collector.addEvent(Event(EventSource.FLOW_NODE, EventType.END, it.value.elementId,
-            it.value.bpmnElementType.elementTypeName.orElseGet { "" }, it.value.bpmnProcessId, it.timestamp)) }
+        .filter { it.value.bpmnElementType != BpmnElementType.PROCESS }
+        .mapNotNull { mapEvent(it) }
+        .forEach { collector.addEvent(it) }
+}
+
+/**
+ * Maps a record in the stream to an event or null, if the record is not of importance for the coverage.
+ */
+fun mapEvent(record: io.camunda.zeebe.protocol.record.Record<ProcessInstanceRecordValue>): Event? {
+    val eventSource =
+        if (record.value.bpmnElementType == BpmnElementType.SEQUENCE_FLOW) EventSource.SEQUENCE_FLOW
+        else EventSource.FLOW_NODE
+    val eventType =
+        if (record.value.bpmnElementType == BpmnElementType.SEQUENCE_FLOW) EventType.TAKE
+        else if (record.intent == ProcessInstanceIntent.ELEMENT_ACTIVATED) EventType.START
+        else if (record.intent == ProcessInstanceIntent.ELEMENT_COMPLETED) EventType.END
+        else null
+    return eventType?.let { Event(eventSource, it, record.value.elementId,
+        record.value.bpmnElementType.elementTypeName.orElseGet { "" }, record.value.bpmnProcessId, record.timestamp) }
 }
