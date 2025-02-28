4838b5463276f4ff2644b821bbdd59d4b4cbde53
Oleg Skrypnyuk <oleg.skrypnyuk@cib.de>
2025-02-28 18:52:05 +0100

    Compatibility check Github Action (#3)
    * preparing for compatibility check
    
    * using corrent parent version
    
    * using correct parent versions
    
    * using cibseven-engine-spring dependency
    
    * switched from javax to jakarta

--- regression-tests/junit4-platform-7/src/test/java/org/cibseven/community/process_test_coverage/junit4/platform7/spring/SpringProcessWithCoverageTest.java
+++ regression-tests/junit4-platform-7/src/test/java/org/cibseven/community/process_test_coverage/junit4/platform7/spring/SpringProcessWithCoverageTest.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 import java.util.HashMap;
 import java.util.Map;
