a404e5132d698d07e187d7f034f139a556e1e8a1
Jan Rohwer <jan@rohwer.online>
2025-08-04 07:40:16 +0200

    feat: #655 package updates and single file packaging

--- extension/report-generator/vite.config.js
+++ extension/report-generator/vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
     root: 'src/main/frontend',
     base: './',
-    plugins: [react()],
+    plugins: [
+        react({
+            jsxImportSource: 'react',
+            babel: {
+                plugins: ['@babel/plugin-transform-react-constant-elements']
+            }
+        }),
+        viteSingleFile()
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src/main/frontend'),
@@ -14,6 +23,15 @@ export default defineConfig({
     build: {
         outDir: '../../../target/classes',
         emptyOutDir: false,
+        minify: 'terser',
+        cssCodeSplit: false,
+        assetsInlineLimit: Infinity,
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            }
+        }
     },
     server: {
         port: 3000,
